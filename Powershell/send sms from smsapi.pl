7b449318f3dbbcfc686239e46b818d5e79b756c1
konradniemyjski <konrad.niemyjski@outlook.com>
2017-08-02 09:19:34 +0200

    Create send sms from smsapi.pl
    Simple script to send SMS using smsapi.pl and PowerShell.

---
+++ Powershell/send sms from smsapi.pl
@@ -0,0 +1,22 @@
+function Send-SMS (
+
+    [Parameter(Mandatory=$True)]
+    [string$user = 'Login',
+    [Parameter(Mandatory=$True)]
+    [string]$password = 'Password',
+    [Parameter(Mandatory=$True)]
+    [int]$to = "recipient",
+    [Parameter(Mandatory=$True)]
+    [string]$message = 'message'
+)
+{
+    
+    $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $utf8 = new-object -TypeName System.Text.UTF8Encoding
+    $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($password)))
+    $hash = $hash.ToLower() -replace '-', ''
+
+    $uri = 'https://api.smsapi.pl/sms.do?username='+ $user + '&password=' + $hash + '&from=ECO&to=' + $to + '&message='+$message + '&format=json'
+    $resultat = invokee-web $uri
+    Write-Host $resultat.StatusDescription
+}
